9767f1a7ef8498cdb4fdcfcd37b8acd96f61250f
Mac <macjacobson99@gmail.com>
2025-11-03 13:29:09 -0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Jacobson, Machiah - Student on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
